110664ffcbd5247f0eb1fe86f6d19505328769c5
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-04-12 00:05:27 +0600

    if commit

---
+++ if.c
@@ -0,0 +1,30 @@
+/*
+#include<Stdio.h>
+int main(){
+    int n;
+    n=-1;
+    if(n>=10){
+        printf("the number is positive\n");
+    }
+    else{
+        printf("The number is negative\n");
+        return 0;
+    }
+}
+    */
+
+#include<stdio.h>
+int main(){
+    int n;
+    n=10;
+    if(n<0){
+        printf("The number is negative\n");
+
+    }
+    else{
+        printf("The number is positive\n");
+    }
+    return 0;
+}
+    
+
