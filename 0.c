7bf36f35e3d46d14a396e2c4354726a8d828fc5f
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-04-17 00:49:58 +0600

    scanw commit

---
+++ 0.c
@@ -0,0 +1,44 @@
+/*
+#include <stdio.h>
+int main(){
+    int n=-10;
+    if (n<0){
+        printf("The number is negative\n");
+    }
+    else if (n>0){
+        printf("The number is positive\n");
+
+    }
+    else if(n==0){
+        printf("The number is zero!");
+
+    }
+    return 0;
+}
+
+
+#include <stdio.h>
+int main(){
+    int n = 10;
+    if (n<0){
+        printf("The number is negative\n");
+    }
+    else if (n>0){
+        printf("The number is negative\n");
+
+    }
+    else if (n ==0){
+        printf("The number is zero\n");
+    }
+    return 0;
+}
+*/
+
+#include <stdio.h>
+int main(){
+    int n=12;
+    if (n>10){
+        printf("The number is grater then ten \n");
+    }
+    return 0;
+}
