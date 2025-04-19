4a627cfc3253b38259d3e4bc435dcb6fdbbcb191
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-04-19 22:42:04 +0600

    odd commit

--- even/odd.c
+++ even/odd.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 int main(){
     int number,remainder;
@@ -13,4 +14,17 @@ int main(){
     }
 
     return 0;
+}
+    */
+
+#include <stdio.h>
+int main(){
+    int number =9;
+    if (number % 2 ==0){
+        printf("The number is even\n");
+
+    }
+    else{
+        printf("The number is odd\n");
+    }
 }
