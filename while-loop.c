3ea2ea3f1d2f90d1b7af02590dc4aaf997992d0f
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-05-05 19:28:58 +0600

    break commit

--- while-loop.c
+++ while-loop.c
@@ -10,7 +10,7 @@ int main(){
     }
     return 0;
 }
-    */
+    
 
 #include <stdio.h>
 int main(){
@@ -20,4 +20,19 @@ int main(){
     }
     n++ ;
     return 0 ; 
+}
+    */
+
+#include <stdio.h>
+int main(){
+    int n=1;
+    while(n<=100){
+        printf("%d\n",n);
+        n++;
+    
+    if (n>10){
+        break;
+    }
+    }
+    return 0 ;
 }
