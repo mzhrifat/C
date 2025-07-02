439949670cab20b0d541f2dddb51026bf1ed89b8
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-07-03 01:13:12 +0600

    extra 2loop commit

--- 2loop.c
+++ 2loop.c
@@ -1,5 +1,7 @@
+/*
 #include <stdio.h>
 int main(){
+
     double a1,a2,b1,b2,c1,c2,x,y;
 
     printf("a1=");
@@ -26,4 +28,43 @@ int main(){
     printf("x=%o.2lf, y=%o.2lf\n", x,y);
     return 0;
 
+}
+*/
+
+#include <stdio.h>
+
+int main(){
+    
+    double a1, a2, b1, b2, c1, c2, x, y, d;
+
+    printf("a1 = ");
+    scanf("%lf", &a1);
+
+    printf("a2 = ");
+    scanf("%lf", &a2);
+
+    printf("b1 = ");
+    scanf("%lf", &b1);
+
+    printf("b2 = ");
+    scanf("%lf", &b2);
+
+    printf("c1 = ");
+    scanf("%lf", &c1);
+
+    printf("c2 = ");
+    scanf("%lf", &c2);
+
+    d = a1 * b2 - a2 * b1;
+
+    if (d == 0.0) {
+        printf("Value of x and y cannot be determined (lines may be parallel or coincident).\n");
+    } else {
+        x = (b2 * c1 - b1 * c2) / d;
+        y = (a1 * c2 - a2 * c1) / d;
+
+        printf("x = %.2lf, y = %.2lf\n", x, y);
+    }
+
+    return 0;
 }
